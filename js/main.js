f167e9dc0867c1fe60a65f7adcd554071e08649f
Michael Mensinger <mikal815@gmail.com>
2018-03-06 18:58:24 -0500

    made a ton of additions and got jquery to work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
 console.log("working")
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+//$(document).ready(function(){
+
+
+  var coolButton = document.getElementById("btn1");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn2");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn3");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn4");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn5");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn6");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn7");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn8");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn9");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn10");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn11");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+  var coolButton = document.getElementById("btn12");
+coolButton.addEventListener('click', function(){
+  alert('Item added to cart');
+})
+
+
+
+
+
+
+
+});
